4251f506a1e00d799467da60736e713a124073b8
denisrenanlinden <88343413+denisrenanlinden@users.noreply.github.com>
2025-04-09 21:09:02 -0300

    updates

--- produtos/views.py
+++ produtos/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Produto
 
 # Create your views here.
 
@@ -8,9 +9,12 @@ def index(request):
 
 
 def produtos(request):
+    # Vamos pegar todos os produtos do banco de dados
+    lista_produtos = Produto.objects.all()
     dados_index = {
         'frase': 'Frase para testar lá no Frontend',
         'nome': 'Lucas',
         'idade': 25,
+        'produtos': lista_produtos,  # Passando a lista de produtos para o dicionario
     }
     return render(request, 'produtos/produtos.html', dados_index)
